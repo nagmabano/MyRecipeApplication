fa61240127d7ef0c83bf73521604ee364f1eba96
Nagma Bano <nagma.bano@sap.com>
2024-03-11 17:14:54 +0530

    boilerplate

---
+++ app/src/main/java/com/nagma/myrecipeapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nagma.myrecipeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
