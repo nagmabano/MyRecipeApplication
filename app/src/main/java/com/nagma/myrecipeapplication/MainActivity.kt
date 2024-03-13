b40c6d7e30d71571ae7c8ea34f0c59ed099d976c
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-13 11:38:37 +0530

    created a binding and added a fragment using supportFragmentManager

--- app/src/main/java/com/nagma/myrecipeapplication/MainActivity.kt
+++ app/src/main/java/com/nagma/myrecipeapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nagma.myrecipeapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.nagma.myrecipeapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        // created the binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Adding the Fragment in this view
+        supportFragmentManager.commit {
+            add<HomeFragment>(R.id.container, null)
+        }
+
+
+
     }
 }
