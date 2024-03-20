f6814c1d8942ebd1b88acc691e596c79be08a6f9
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-20 14:27:12 +0530

    recycler view with generic list adapter

--- app/src/main/java/com/nagma/myrecipeapplication/ProductsFragment.kt
+++ app/src/main/java/com/nagma/myrecipeapplication/ProductsFragment.kt
@@ -1,32 +1,25 @@
 package com.nagma.myrecipeapplication
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.nagma.myrecipeapplication.data.Product
+import com.nagma.myrecipeapplication.data.ProductRepository
 import com.nagma.myrecipeapplication.databinding.FragmentProductsBinding
-import com.nagma.myrecipeapplication.databinding.ItemBinding
-import java.lang.StringBuilder
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProductsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,14 +30,6 @@ class ProductsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding_product = FragmentProductsBinding.inflate(layoutInflater)
-
-        val viewModel by viewModels<MainViewModel>()
-
-        viewModel.products.observe(viewLifecycleOwner) {products ->
-            binding_product.productList.adapter = ProductAdapter(products)
-        }
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_products, container, false)
     }
@@ -52,34 +37,28 @@ class ProductsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val product = Product(
-            name = "Item",
-            imageFile = "image_file",
-            description = "My description",
-            size = 200,
-            price = 12.34
-        )
+        val viewModel: MainViewModel by viewModels()
 
-        Log.i("Product", "My Product: $product" )
-    }
+        val product_list = view.findViewById<RecyclerView>(R.id.product_list)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        viewModel.products.observe(viewLifecycleOwner) { products ->
+            product_list.adapter = object : GenericListAdapter<Product>(
+                R.layout.item,
+                bind = {item, holder, itemCount ->
+                    with(holder.itemView) {
+                        this.findViewById<TextView>(R.id.productNameText).text = item.name
+                        this.findViewById<ImageView>(R.id.productImage).load(item.imageFile) {
+                            crossfade(100)
+                        }
+                        this.findViewById<TextView>(R.id.sizeText).text = item.size.toString()
+                        this.findViewById<TextView>(R.id.priceText).text = item.price.toString()
+                    }
                 }
+            ) {}.apply {
+                submitList(
+                    products
+                )
             }
+        }
     }
 }
